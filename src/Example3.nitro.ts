786766c0bb358a6783405022da81ca70e9f756e8
VLS <vini_ls@hotmail.com>
2026-06-14 20:01:36 -0300

    fixing lint

--- src/Example3.nitro.ts
+++ src/Example3.nitro.ts
@@ -8,7 +8,7 @@ export interface Example3Props extends HybridViewProps {
   color: string;
 }
 export interface Example3Methods extends HybridViewMethods {}
-
+// prettier-ignore
 export type Example3 = HybridView<
   Example3Props,
   Example3Methods
--- src/Example3View.native.tsx
+++ src/Example3View.native.tsx
@@ -1,10 +1,11 @@
 import { getHostComponent } from 'react-native-nitro-modules';
 const Example3Config = require('../nitrogen/generated/shared/json/Example3Config.json');
+// prettier-ignore
 import type {
   Example3Methods,
   Example3Props,
 } from './Example3.nitro';
-
+// prettier-ignore
 export const Example3View = getHostComponent<
   Example3Props,
   Example3Methods
